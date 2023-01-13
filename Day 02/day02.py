cf6c833af505d6311535b49e10da89ef4bb18343
unknown <bikihy@naver.com>
2023-01-13 14:43:26 +0900

    day02 number2

--- Day 02/day02.py
+++ Day 02/day02.py
@@ -1,13 +1,30 @@
-harry_potter = {'헤르미온느':'그리핀도르','말포이':'슬리데린','루나 러브굿':'래번클로'}
-print('harry_potter = ',harry_potter)
-print('type(harry_potter) = ',type(harry_potter))
+#harry_potter = {'헤르미온느':'그리핀도르','말포이':'슬리데린','루나 러브굿':'래번클로'}
+#print('harry_potter = ',harry_potter)
+#print('type(harry_potter) = ',type(harry_potter))
 
-PI = 3.14
-print(f'원주율의 값은 {PI}이고 타입은 {type(PI)}입니다.')
+#PI = 3.14
+#print(f'원주율의 값은 {PI}이고 타입은 {type(PI)}입니다.')
 
-print('2*2*2*2*2 = 2**5 = pow(2,5)',2*2*2*2*2)
-print(divmod(9,5))
+#print('2*2*2*2*2 = 2**5 = pow(2,5)',2*2*2*2*2)
+#print(divmod(9,5))
 
-test = 1, 2, 3 #packing
-print(type(test))
-print(test[3])
+#test = 1, 2, 3 #packing
+#print(type(test))
+#print(test[3])
+
+# Chapter 3
+
+# number = 0b10011010 - 2진법
+# number = 0x9A       - 16진법
+# number = 0o232      - 8진법
+
+value = 65
+print('bin(65) = ',bin(value))
+print('oct(65) = ',oct(value))
+
+# 헥스 코드
+print('hex(65) = ',hex(value))
+
+# 아스키 코드
+print('chr(65) =',chr(65))
+print('ord("A") = ',ord('A'))
---
+++ Day 02/day02_2.py
@@ -0,0 +1,24 @@
+# fahrenheit = float(input('화씨 온도 : '))
+# celsius = (fahrenheit - 32.0) * (5.0/9.0)
+# print(f'화씨 온도 {fahrenheit}도는 섭씨 온도 {celsius:.4f}입니다.')
+
+furry = True
+large = True
+if furry:
+    if large:
+        print("It's a yeti.")
+    else:
+        print("It's a cat.")
+else:
+    if large:
+        print("It's a whale.")
+    else:
+        print("It's a human.")
+
+x = 7
+print(5 < x < 10)
+
+# False
+print(bool(0))
+
+print(ord("a"))
