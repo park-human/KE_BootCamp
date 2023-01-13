84306d778f82a819a36f3532d4bef99aba0142c4
unknown <bikihy@naver.com>
2023-01-13 13:36:50 +0900

    update

--- Day 02/day02.py
+++ Day 02/day02.py
@@ -1,3 +1,13 @@
 harry_potter = {'헤르미온느':'그리핀도르','말포이':'슬리데린','루나 러브굿':'래번클로'}
 print('harry_potter = ',harry_potter)
 print('type(harry_potter) = ',type(harry_potter))
+
+PI = 3.14
+print(f'원주율의 값은 {PI}이고 타입은 {type(PI)}입니다.')
+
+print('2*2*2*2*2 = 2**5 = pow(2,5)',2*2*2*2*2)
+print(divmod(9,5))
+
+test = 1, 2, 3 #packing
+print(type(test))
+print(test[3])
