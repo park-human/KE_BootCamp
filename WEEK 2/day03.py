ebfa3e6872f58cecf0b212947812f9677daa5ff0
unknown <bikihy@naver.com>
2023-01-16 09:27:00 +0900

    230116 9:26AM Updated

---
+++ WEEK 2/day03.py
@@ -0,0 +1,10 @@
+# import random
+# secret = random.randint(1,10)
+# guess = int(input('정답은? :'))
+# if secret == guess:
+#     print('정답입니다.')
+# elif secret > guess:
+#     print(f'정답보다 작은 수입니다. 정답은 {secret}.')
+# else:
+#     print(f'정답보다 큰 수입니다. 정답은 {secret}')
+
