f2b665ad7b830a24e5b90b7f5bbe2b6c5de6bf45
unknown <bikihy@naver.com>
2023-01-19 13:41:03 +0900

    week2 day06 exception

--- WEEK 2/week2_day06.py
+++ WEEK 2/week2_day06.py
@@ -92,6 +92,14 @@
 # print(change_local())
 # print('globals:',globals())
 
+# def something(n):
+#     a=5
+#     a=a+n
+#     print(a)
+#
+# something(1)
+# something(3)
+
 #recursion
 # def factorial_iter(n):
 #     """
@@ -117,3 +125,35 @@
 #
 # print(factorial_iter(5))
 # print(factorial_recu(5))
+
+# def div_calc(a,b):
+#     """
+#     나누기 함수
+#     :param a: 분자
+#     :param b: 분모
+#     :return: 계산결과
+#     """
+#     return a / b
+#
+# print(div_calc(15,3))
+# try:
+#     (div_calc(15,0))
+# except:
+#     print('0으로는 나눌 수 없습니다.')
+
+try:
+    expr = input('분자와 분모를 입력하세요 : ').split()
+    print(int(expr[0]) / int(expr[1]))
+
+except ValueError as err:
+    print(f'숫자를 입력해주세요. ({err})')
+except ZeroDivisionError as err:
+    print(f'분모에 0이 올 수 없습니다.({err})')
+except IndexError as err:
+    print(f'입력 값의 범위에 문제가 있습니다.({err})')
+except Exception as other:
+    print(f'예외 발생 : {other}')
+else: # 예외가 발생하지 않았을 때
+    print('프로그램 정상', end=' ')
+finally: # 예외 발생 여부와 상과없이 무조건 실행
+    print('종료')
