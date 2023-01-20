49cda3d0d4a010e94b6b0d111d320281e4b2c911
unknown <bikihy@naver.com>
2023-01-20 14:50:39 +0900

    duck typing

--- WEEK 2/week2_day07.py
+++ WEEK 2/week2_day07.py
@@ -204,9 +204,39 @@
 # A.kids()
 
 # Duck Typing
+import math
 class Shape:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
 	def get_area(self):
 		print('도형의 면적을 출력합니다.')
 
+
 class Circle(Shape):
-	def
+	def __init__(self, x, y, radius):
+		super().__init__(x,y)
+		self.radius = radius
+
+	def get_area(self):
+		return math.pi * self.radius * self.radius
+
+
+class Rectangle(Shape):
+	def __init__(self, x, y, width, length):
+		super().__init__(x, y)
+		self.width = width
+		self.length = length
+
+	def get_area(self):
+		return self.width * self.length
+
+
+c1 = Circle(100, 100, 10.0)
+c2 = Circle(50, 50, 2.0)
+r1 = Rectangle(100, 50, 5, 2)
+
+print(f'사각형의 좌표는 x:{r1.x}, y:{r1.y}이고 넓이는 {r1.get_area()}입니다.')
+print(c1.get_area())
+print(c2.get_area())
