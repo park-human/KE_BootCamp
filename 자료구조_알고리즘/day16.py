ad6ddee8f563fa380ccf5f41de799b25ca08020f
park-human <122219505+park-human@users.noreply.github.com>
2023-02-06 14:18:29 +0900

    230206 2:18 PM

--- 자료구조_알고리즘/day16.py
+++ 자료구조_알고리즘/day16.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -20,15 +23,15 @@ def insert_nodes(find_data, insert_data):
     if head.data == find_data:
         node = Node(insert_data)
         node.link = head
-        last = head  #
-        while last.link != head:  #
-            last = last.link  #
-        last.link = node  #
+        last = head
+        while last.link != head:
+            last = last.link
+        last.link = node
         head = node
         return
 
     current = head
-    while current.link != head:  #
+    while current.link != head:
         pre = current
         current = current.link
         if current.data == find_data:
@@ -39,7 +42,7 @@ def insert_nodes(find_data, insert_data):
 
     node = Node(insert_data)
     current.link = node
-    node.link = head  #
+    node.link = head
 
 
 def delete_nodes(delete_data):
@@ -48,11 +51,15 @@ def delete_nodes(delete_data):
     if head.data == delete_data:
         current = head
         head = head.link
+        last = head  #
+        while last.link != current:  #
+            last = last.link  #
+        last.link = head  #
         del current
         return
 
     current = head
-    while current.link != None:
+    while current.link != head:
         pre = current
         current = current.link
         if current.data == delete_data:
@@ -68,64 +75,91 @@ def find_nodes(find_data):
     if current.data == find_data:
         return current
 
-    while current.link != head:
+    while current.link != head:  #
         current = current.link
         if current.data == find_data:
             return current
 
     return Node(None)
-
-
-head, current, pre = None, None, None
-data_array = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
 
 
 def is_find(find_data):
     """
-    연결 리스트 안에서 원소 존재 여부 판정 함수
+    연결 리스트안에서 원소 존재 여부 판정 함수
     :param find_data: 찾고자 하는 원소. str
-    :return:연결 리스트 안에서 원소가 존재하면 True 리턴 아니면 False
+    :return: 연결 리스트안에서 원소가 존재하면 True리턴 아니면 False
     """
-    global head, current, pre
-    current = head
 
+    global head, current, pre
+
+    current = head
     if current.data == find_data:
         return True
 
-    while current.data != head:
+    while current.link != head:  #
         current = current.link
         if current.data == find_data:
-            return current
+            return True
 
-    return Node(None)
+    return False
+
+
+def count_odd_even():
+    global head, current
+
+    even, odd = 0, 0
+
+    # SRP 위배
+    # if head == None:
+    #     return False
+
+    current = head
+    while True:
+        if current.data % 2 == 0:
+            even = even + 1
+        else:
+            odd = odd + 1
+        if current.link == head:
+            break
+        current = current.link
+
+    return odd, even
+
+
+def makeSquareNumber(odd, even):
+    if odd > even:
+        remainder = 1
+    else:
+        remainder = 0
+
+    current = head
+    while current.link != head:
+        if current.data % 2 == remainder:
+            current.data = current.data * current.data
+        current = current.link
+    current.data = current.data * current.data
+
+
+head, current, pre = None, None, None
+data_array = list()
 
 if __name__ == "__main__":
+    # odd_even = count_odd_even()  # False 리턴
+    for _ in range(7):
+        data_array.append(random.randint(1, 10))
+
     node = Node(data_array[0])
     head = node
-    node.link = head  #
+    node.link = head
 
     for data in data_array[1:]:
         pre = node
         node = Node(data)
         pre.link = node
-        node.link = head  #
+        node.link = head
 
-    # print_nodes(head)
-    # insert_nodes("피카츄", "잠만보")
-    # print_nodes(head)
-    # insert_nodes("파이리", "어니부기")
-    # print_nodes(head)
-    # insert_nodes("성윤모", "거북왕")
-    # print_nodes(head)
-
-    # print_nodes(head)
-    # delete_nodes("피카츄")
-    # print_nodes(head)
-    # delete_nodes("파이리")
-    # print_nodes(head)
-    # delete_nodes("없는거")
-    # print_nodes(head)
-
-    print(is_find("꼬부기").data)
-    print(find_nodes("꼬부기").data)
-    print(find_nodes("없는거").data)
+    print_nodes(head)
+    odd_even = count_odd_even()
+    print(f'Odd Number : {odd_even[0]}, Even Number {odd_even[1]}')
+    makeSquareNumber(odd_even[0], odd_even[1])
+    print_nodes(head)
