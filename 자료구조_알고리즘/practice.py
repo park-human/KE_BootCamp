fb39d17e35d84417e0a1cc2f517476d1a0905878
park-human <122219505+park-human@users.noreply.github.com>
2023-02-07 15:41:10 +0900

    230207 3:40 PM

---
+++ 자료구조_알고리즘/practice.py
@@ -0,0 +1,17 @@
+# Code 08-06
+
+import random
+
+## 함수 선언 ##
+class TreeNode() :
+    def __init__(self):
+        self.left = None
+        self.data = None
+        self.right = None
+
+## 전역 변수 선언 ##
+memory = []
+rootBook, rootAuth = None, None
+bookAry = [
+
+]
