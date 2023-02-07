cd1120d5e3c842475762fa8e37f9dff2af241413
park-human <122219505+park-human@users.noreply.github.com>
2023-02-07 18:26:10 +0900

    assignment 7-2 complete

--- 자료구조_알고리즘/assignment_day17.py
+++ 자료구조_알고리즘/assignment_day17.py
@@ -1,11 +1,81 @@
+# ## 함수 선언 ##
+# def isQueueFull():
+# 	global SIZE, queue, front, rear
+# 	if (rear == SIZE-1):
+# 		return True
+# 	else:
+# 		return False
+# def isQueueEmpty():
+# 	global SIZE, queue, front, rear
+# 	if (front == rear):
+# 		return True
+# 	else:
+# 		return False
+#
+# def enQueue(data):
+# 	global SIZE, queue, front, rear
+# 	if (isQueueFull()):
+# 		print("큐가 꽉 찼습니다.")
+# 		return
+# 	rear += 1
+# 	queue[rear] = data
+# def deQueue():
+# 	global SIZE, queue, front, rear
+# 	if (isQueueEmpty()):
+# 		print("큐가 비었습니다.")
+# 		return None
+# 	front +=1
+# 	data = queue[front]
+# 	queue[front] = None
+#
+# 	for i in range(front+1, rear+1):
+# 		queue[i-1] = queue[i]
+# 		queue[i] = None
+# 	front = -1
+# 	rear -= 1
+#
+# 	return data
+#
+# def peek():
+# 	global SIZE, queue, front, rear
+# 	if (isQueueEmpty()) :
+# 		print('큐가 비었습니다.')
+# 		return None
+# 	return queue[front+1]
+#
+# # 7장 응용예제 01
+#
+# ## 전역 변수 선언 ##
+# SIZE = 5
+# queue = [None for _ in range(SIZE)]
+# front = rear = -1
+#
+# ## 메인 코드 ##
+# if __name__ == "__main__":
+# 	enQueue('손님1')
+# 	enQueue('손님2')
+# 	enQueue('손님3')
+# 	enQueue('손님4')
+# 	enQueue('손님5')
+# 	print('대기 줄 상태 : ',queue)
+#
+# 	for _ in range(rear+1):
+# 		print(deQueue(), '님 식당에 입장')
+# 		print('대기 줄 상태 : ', queue)
+#
+# 	print('영업 종료')
+
+# 7장 응용예제 02
+
 ## 함수 선언 ##
-def isQueueFull():
+def is_queue_full():
 	global SIZE, queue, front, rear
-	if (rear == SIZE-1):
+	if ((rear+1) % SIZE == front) :
 		return True
 	else:
 		return False
-def isQueueEmpty():
+
+def is_queue_empty():
 	global SIZE, queue, front, rear
 	if (front == rear):
 		return True
@@ -14,55 +84,50 @@ def isQueueEmpty():
 
 def enQueue(data):
 	global SIZE, queue, front, rear
-	if (isQueueFull()):
+	if is_queue_full():
 		print("큐가 꽉 찼습니다.")
 		return
-	rear += 1
+	rear = (rear+1) % SIZE
 	queue[rear] = data
+
 def deQueue():
 	global SIZE, queue, front, rear
-	if (isQueueEmpty()):
-		print("큐가 비었습니다.")
+	if (is_queue_empty()):
+		print('큐가 비었습니다.')
 		return None
-	front +=1
+	front = (front+1) % SIZE
 	data = queue[front]
 	queue[front] = None
-
-	for i in range(front+1, rear+1):
-		queue[i-1] = queue[i]
-		queue[i] = None
-	front = -1
-	rear -= 1
-
 	return data
 
 def peek():
 	global SIZE, queue, front, rear
-	if (isQueueEmpty()) :
-		print('큐가 비었습니다.')
+	if (is_queue_empty()):
+		print("큐가 비었습니다.")
 		return None
-	return queue[front+1]
+	return queue[(front+1) % SIZE]
 
-# 7장 응용예제 01
+def timeLeft():
+	global SIZE, queue, front, rear
+	timeSum = 0
+	for i in range((front+1) % SIZE, (rear+1)%SIZE):
+		timeSum += queue[i][1]
+	return timeSum
 
 ## 전역 변수 선언 ##
-SIZE = 5
+SIZE = 6
 queue = [None for _ in range(SIZE)]
-front = rear = -1
+front = rear = 0
 
 ## 메인 코드 ##
 if __name__ == "__main__":
-	enQueue('손님1')
-	enQueue('손님2')
-	enQueue('손님3')
-	enQueue('손님4')
-	enQueue('손님5')
-	print('대기 줄 상태 : ',queue)
+	waitTime = [('사용', 9), ('고장', 3), ('환불', 4), ('환불', 4), ('고장', 3)]
 
-	for _ in range(rear+1):
-		print(deQueue(), '님 식당에 입장')
-		print('대기 줄 상태 : ', queue)
+	for time in waitTime:
+		print('귀하의 대기 예상 시간은 ', timeLeft(), '분입니다.')
+		print('현재 대기 콜 ->', queue)
+		enQueue(time)
+		print()
 
-	print('영업 종료')
-
-# 7장 응용예제 02
+	print('최종 대기 콜 : ', queue)
+	print('프로그램 종료')
