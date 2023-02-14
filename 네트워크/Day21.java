a365bf0a5a7781ba62974439d108daca7b089a77
park-human <122219505+park-human@users.noreply.github.com>
2023-02-14 10:21:22 +0900

    230214 Network

---
+++ 네트워크/Day21.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Day21 {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://google.com");
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream())
+            );
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (MalformedURLException e){
+            throw new RuntimeException(e);
+        } catch (IOException ex) {
+            System.out.println('a');
+        }
+    }
+}
