a6328944b35528bcf45d700111cedcfa1b521fea
park-human <122219505+park-human@users.noreply.github.com>
2023-02-14 13:14:22 +0900

    230214 1:14 PM

--- 네트워크/SimpleEchoServer.java
+++ 네트워크/SimpleEchoServer.java
@@ -4,31 +4,46 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+// IP 165.246.115.165 포트 20000
 
-public class SimpleEchoServer {
+public class SimpleEchoServer implements Runnable {
+    // 다중 접속 에코 서버
+    private static Socket clientSocket;
+
+    public SimpleEchoServer(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
     public static void main(String[] args) {
-        System.out.println("에코 서버");
-        try (ServerSocket serverSocket = new ServerSocket(6000)) {
-            System.out.println("클라이언트 접속 대기 중.....");
-            Socket clientSocket = serverSocket.accept(); // 접속 대기
-            System.out.println("클라이언트에 접속됨");
-
-            BufferedReader br = null;
-            PrintWriter pw = null;
-            try {
-                br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                pw = new PrintWriter(clientSocket.getOutputStream(), true);
-                String line;
-                while ((line = br.readLine()) != null) {
-                    System.out.println("클라이언트로부터 받은 메세지: " + line);
-                   pw.println(line); // 클라이언트로 송신
-                }
-            } catch(IOException e){
-                throw new RuntimeException(e);
-            } finally{
-                if(br != null) br.close();
-                if(pw != null) pw.close();
+        System.out.println("다중 접속 에코 서버");
+        try (ServerSocket serverSocket = new ServerSocket(20000)) {
+            while (true) {
+                System.out.println("Waiting for connection...");
+                clientSocket = serverSocket.accept();
+                SimpleEchoServer tes = new SimpleEchoServer(clientSocket);
+                new Thread(tes).start();
             }
+        } catch (IOException ex) {
+            // Handle exceptions
         }
-        catch (IOException ex) {
-        System.out.println("접속 실패!");}}}
+        System.out.println("Threaded Echo Server Terminating");
+    }
+
+    @Override
+    public void run() {System.out.println(Thread.currentThread() + " 스레드 접속");
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
+        ) {
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                System.out.println("["+ Thread.currentThread() + "]" + " 클라이언트가 보낸 메세지 : " + inputLine);
+                out.println(inputLine);
+            }
+            System.out.println("[" + Thread.currentThread() +"]"+" 클라이언트가 종료됨");}
+        catch (IOException ex)
+        {
+            System.out.println("입출력 예외 발생");
+        }
+    }
+}
