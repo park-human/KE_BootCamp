d6dc8146585bd0c4c7822c92a9e9ac5d9a230518
park-human <122219505+park-human@users.noreply.github.com>
2023-02-15 15:40:29 +0900

    230215 3:40 PM

--- 네트워크/Test.java
+++ 네트워크/Test.java
@@ -1,19 +1,48 @@
-// StringBuilder vs String
-// string append
-public class Test {
-    public static void main(String[] args){
-        String s = new String("hi");
-        long start = System.nanoTime();
-        for (int i = 1; i<1000 ; i++)
-            s = s + "!";
-        long end = System.nanoTime();
-        System.out.println(end - start);
-
-        StringBuilder sb = new StringBuilder("hi");
-        start = System.nanoTime();
-        for (int i = 1; i < 1000 ; i++)
-            sb = sb.append("!");
-        end = System.nanoTime();
-        System.out.println(end - start);
+class One{
+    public One(){
+    }
+    public static One produceOneInstance(){
+        return new One();
     }
 }
+
+public class Test{
+    public static void main(String[] args){
+        // One o = new One
+        One o1 = One.produceOneInstance();
+        One o2 = One.produceOneInstance();
+    }
+}
+
+//import java.net.*;
+//
+//// StringBuilder vs String
+//// string append
+//
+//public class Test{
+//    public static String getMACIdentifier(NetworkInterface network) {
+//        StringBuilder identifier = new StringBuilder();
+//        try {
+//            byte[] macBuffer = network.getHardwareAddress();
+//            if (macBuffer != null) {
+//                for (int i = 0; i < macBuffer.length; i++) {
+//                    identifier.append(
+//                            String.format("%02X%s",macBuffer[i],
+//                                    (i < macBuffer.length - 1)
+//                                            ? "-" : ""));
+//                }
+//            } else {
+//                return "---";
+//            }
+//        } catch (SocketException ex) {
+//            ex.printStackTrace();
+//        }
+//        return identifier.toString(); // 스트링빌더 객체를 문자열로 변환 후 리턴
+//    }
+//    public static void main(String[] args) throws UnknownHostException, SocketException {
+//        InetAddress addr = InetAddress.getLocalHost();
+//        System.out.println("IP 주소 : " + addr.getHostAddress());
+//        NetworkInterface network = NetworkInterface.getByInetAddress(addr);
+//        System.out.println("MAC 주소 : " + getMACIdentifier(network));
+//    }
+//}
