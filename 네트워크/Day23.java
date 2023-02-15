d437d66d578d55b1cb89e7b0bee111871b81a97b
park-human <122219505+park-human@users.noreply.github.com>
2023-02-15 11:34:26 +0900

    Diablo v0.3

--- 네트워크/Day23.java
+++ 네트워크/Day23.java
@@ -1,9 +1,16 @@
 public class Day23 {
-    // Diablo v0.2
+    // Diablo v0.3
     public static void main(String[] args){
+        Barbarian b1 = new Barbarian();
         Sorceress s1 = new Sorceress();
         Bow windForce = new Bow();
-        s1.setWeapon(windForce);
+        Axe berserkerAxe = new Axe();
+        b1.setWeapon(berserkerAxe);
+        s1.setWeapon(windForce); // get Weapon
         s1.performWeapon();
+        b1.performWeapon();
+        s1.setWeapon(new Axe());
+        s1.performWeapon();
+        s1.info();
     }
 }
